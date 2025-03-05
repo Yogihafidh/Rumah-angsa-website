37d1778b13d6eb598ab7c206ebd9826ecae19aee
Yogihafidh <hafidhyogi@gmail.com>
2025-03-05 20:59:57 +0700

    feat: add guest area

--- app/account/page.js
+++ app/account/page.js
@@ -3,5 +3,9 @@ export const metadata = {
 };
 
 export default function page() {
-  return <div>Your account</div>;
+  return (
+    <div className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Yogi
+    </div>
+  );
 }
